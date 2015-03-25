eb2dfde6d53df4aac34a33de0c575b21f7ac9cf5
kevin-nottberg <KN12599@gmail.com>
2015-03-25 11:56:32 -0700

    Turning included

--- FRC2015/src/org/usfirst/frc/team1977/robot/commands/auto/PickUpMove.java
+++ FRC2015/src/org/usfirst/frc/team1977/robot/commands/auto/PickUpMove.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1977.robot.commands.auto;
 
 import org.usfirst.frc.team1977.robot.commands.drive.DriveStop;
 import org.usfirst.frc.team1977.robot.commands.drive.DriveTime;
+import org.usfirst.frc.team1977.robot.commands.drive.TurnTime;
 import org.usfirst.frc.team1977.robot.commands.grasper.FireGrasper;
 import org.usfirst.frc.team1977.robot.commands.lift.RaiseLift;
 
@@ -12,14 +13,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class PickUpMove extends CommandGroup {
     
-	// Picks up tote and moves it to auto zone
+	// Picks up tote, turn and moves it to auto zone
 	// Time needs to be adjusted
 	// Speed needs to be adjusted as well
     public PickUpMove() {
-       addSequential( new DriveTime( 0, 0.5, 500 ) );
        addSequential( new FireGrasper() );
-       addParallel( new DriveTime(0, 1, 1500) );
        addSequential( new RaiseLift() );
+       addSequential( new TurnTime( 1000 ) );
+       addSequential( new DriveTime( 0, 1, 15000 ) );
     }
     
     protected void end() {
